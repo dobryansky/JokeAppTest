51795e210a72a2e2b52b014a01238068ec9022a7
Artem <mikstrip@yandex.ru>
2022-02-23 23:02:33 +0300

    Added new thread()

--- app/src/main/java/com/artem/jokeapptest/TestModel.kt
+++ app/src/main/java/com/artem/jokeapptest/TestModel.kt
@@ -6,23 +6,25 @@ class TestModel() : Model<Any, Any> {
     private var count = 1
 
 
-
     override fun getJoke() {
-        Thread.sleep(1000)
-        if (count % 2 == 0) {
-            callback?.provideSuccess("success")
-        } else {
-            callback?.provideError("error")
+        Thread {
+
+            Thread.sleep(1000)
+            if (count % 2 == 0) {
+                callback?.provideSuccess("success")
+            } else {
+                callback?.provideError("error")
+            }
+            count++
+
+        }.start()
+    }
+
+        override fun init(callback: ResultCallback<Any, Any>) {
+            this.callback = callback
         }
-        count++
 
+        override fun clear() {
+            callback = null
+        }
     }
-
-    override fun init(callback: ResultCallback<Any, Any>) {
-        this.callback = callback
-    }
-
-    override fun clear() {
-        callback = null
-    }
-}
